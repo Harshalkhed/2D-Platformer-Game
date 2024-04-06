7bf7803a460d23b30d9abffb981c7881b9c94d01
Harshal S Khedekar <hskhedekar@gmail.com>
2024-04-06 10:23:46 +0530

    Player Controller Script
    Added RidgidBody collider to the player ,Animation script to triggger run animation for the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator playerAnimator;
+    private void Awake()
+    {
+        
+    }
+
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    Debug.Log("Collsion"+ collision.gameObject.name);
+    //}
+
+
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+
+        playerAnimator.SetFloat("Speed",Mathf.Abs( speed));
+        Vector3 scale = transform.localScale;
+        if (speed<0)
+        {
+            
+            scale.x = -1f * Mathf.Abs(scale.x);
+
+        }
+        else if(speed> 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
+
+        
+    }
+
+
+
+
+
+
+
+
+}
